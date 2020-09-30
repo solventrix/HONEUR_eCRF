5b337825382ace6c1f36910137967b09ab9c3669
fredkingham <fred.kingham@openhealthcare.org.uk>
2020-09-30 15:32:58 +0100

    Adds tests for the new api end point

--- entrytool/tests.py
+++ entrytool/tests.py
@@ -1,20 +1,32 @@
 """
 Unittests for entrytool
 """
+from django.urls import reverse
+from entrytool.episode_categories import LineOfTreatmentEpisode
 from opal.core.test import OpalTestCase
 
-from entrytool import models, patient_lists
 
+class CreateNewTestCase(OpalTestCase):
+    def setUp(self):
+        super().setUp()
+        self.patient, _ = self.new_patient_and_episode_please()
 
-class WeHaveSomeModelsTestCase(OpalTestCase):
-    def test_there_is_a_model(self):
-        demographics = models.Demographics(first_name="Larry")
-        self.assertEqual("Larry", demographics.first_name)
-
-
-class PatientListTestCase(OpalTestCase):
-    def test_queryset(self):
-        all_patients = patient_lists.AllPatientsList()
-        self.assertEqual(0, all_patients.get_queryset().count())
-        patient, episode = self.new_patient_and_episode_please()
-        self.assertEqual(1, all_patients.get_queryset().count())
+    def test_create_new_lot(self):
+        url = reverse(
+            "new_line_of_treatment_episode-detail",
+            kwargs={"pk": self.patient.pk}
+        )
+        self.assertTrue(
+            self.client.login(
+                username=self.user.username, password=self.PASSWORD
+            )
+        )
+        response = self.client.put(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            self.patient.episode_set.all().count(), 2
+        )
+        self.assertEqual(
+            self.patient.episode_set.last().category_name,
+            LineOfTreatmentEpisode.display_name
+        )
