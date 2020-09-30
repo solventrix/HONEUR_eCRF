eb9a2a3a492d00bbf7e0a141281b00aba7d2bd6a
fredkingham <fred.kingham@openhealthcare.org.uk>
2020-09-30 16:13:22 +0100

    Move the patient lists and the hospitals.json into the dev app, in production we expect these to be populated on a per app basis

---
+++ dev/urls.py
@@ -0,0 +1,10 @@
+"""
+Urls for the dev Opal plugin
+"""
+from django.conf.urls import url
+
+from dev import views
+
+urlpatterns = [
+    #url(pattern, view)
+]
---
+++ dev/views.py
@@ -0,0 +1,4 @@
+"""
+Views for the dev Opal Plugin
+"""
+# from django.views.generic import View
