7d8eac123c4c0dbad5c68aee1389536593ae0b3c
David Miller <david@deadpansincerity.com>
2020-09-30 16:27:40 +0100

    Remove plugin noise

--- dev/plugin.py
+++ dev/plugin.py
@@ -3,31 +3,8 @@ Plugin definition for the dev Opal plugin
 """
 from opal.core import plugins
 
-from dev.urls import urlpatterns
 
 class DevPlugin(plugins.OpalPlugin):
     """
     Main entrypoint to expose this plugin to our Opal application.
     """
-    urls = urlpatterns
-    javascripts = {
-        # Add your javascripts here!
-        'opal.dev': [
-            # 'js/dev/app.js',
-            # 'js/dev/controllers/larry.js',
-            # 'js/dev/services/larry.js',
-        ]
-    }
-
-    def list_schemas(self):
-        """
-        Return any patient list schemas that our plugin may define.
-        """
-        return {}
-
-    def roles(self, user):
-        """
-        Given a (Django) USER object, return any extra roles defined
-        by our plugin.
-        """
-        return {}
--- dev/views.py
+++
@@ -1,4 +0,0 @@
-"""
-Views for the dev Opal Plugin
-"""
-# from django.views.generic import View
