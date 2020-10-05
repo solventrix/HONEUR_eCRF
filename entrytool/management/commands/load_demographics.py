26dd1f6cc0c5e0f5e3fd81f89c5af29b6af4bcae
fredkingham <fred.kingham@openhealthcare.org.uk>
2020-10-05 16:08:21 +0100

    To improve readability add a mapping at the top of the file

--- entrytool/management/commands/load_demographics.py
+++ entrytool/management/commands/load_demographics.py
@@ -8,6 +8,30 @@ from entrytool.models import PatientDetails, Hospital
 from opal.models import Patient
 
 
+# field -> csv column title mapping
+field_map = dict(
+
+    # Demographics fields
+    date_of_birth="date_of_birth",
+    hospital_number="Hospital_patient_ID",
+
+    # Patient Detail fields
+    hospital="Hospital",
+    diag_date="date_of_diagnosis",
+    smm_history="SMM_history",
+    smm_history_date="SMM_History_date",
+    mgus_history="MGUS_history",
+    mgus_history_date="MGUS_history_date",
+    r_iss_stage="R_ISS_stage",
+    pp_type="PP_type",
+    del_17p="Del17p",
+    t4_14="t_4_14",
+    t4_16="t_4_16",
+    death_date="date_of_death",
+    death_cause="cause_of_death"
+)
+
+
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("file_name", help="Specify import file")
@@ -21,34 +45,34 @@ class Command(BaseCommand):
                 # skip empty rows
                 if not any(row.values()):
                     continue
-                hospital_number = row["Hospital_patient_ID"].strip()
+                hospital_number = row[field_map["hospital_number"]].strip()
                 if hospital_number:
                     patients = Patient.objects.filter(
                         demographics__hospital_number=hospital_number
                     )
                     if patients.exists():
                         raise ValueError(
-                            "Patient {} already exists".format(hospital_number)
+                            "Patient {} already extitledists".format(hospital_number)
                         )
                 date_of_birth = translate_date(row["date_of_birth"])
 
                 patient_details = {
-                    "hospital": get_or_create_ll(row["Hospital"], Hospital),
-                    "diag_date": translate_date(row["date_of_diagnosis"]),
-                    "smm_history": no_yes_unknown(row["SMM_history"]),
-                    "smm_history_date": translate_date(row["SMM_History_date"]),
-                    "mgus_history": no_yes_unknown(row["MGUS_history"]),
-                    "mgus_history_date": translate_date(row["MGUS_history_date"]),
-                    "r_iss_stage": no_yes_unknown(row["R_ISS_stage"]),
+                    "hospital": get_or_create_ll(row[field_map["hospital"]], Hospital),
+                    "diag_date": translate_date(row[field_map["diag_date"]]),
+                    "smm_history": no_yes_unknown(row[field_map["smm_history"]]),
+                    "smm_history_date": translate_date(row[field_map["smm_history_date"]]),
+                    "mgus_history": no_yes_unknown(row[field_map["mgus_history"]]),
+                    "mgus_history_date": translate_date(row[field_map["mgus_history_date"]]),
+                    "r_iss_stage": no_yes_unknown(row[field_map["r_iss_stage"]]),
                     "pp_type": get_and_check(
-                        row["PP_type"], PatientDetails.PP_TYPE_CHOICES
+                        row[field_map["pp_type"]], PatientDetails.PP_TYPE_CHOICES
                     ),
-                    "del_17p": no_yes_unknown(row["Del17p"]),
-                    "t4_14": no_yes_unknown(row["t_4_14"]),
-                    "t4_16": no_yes_unknown(row["t_4_16"]),
-                    "death_date": translate_date(row["date_of_death"]),
+                    "del_17p": no_yes_unknown(row[field_map["del_17p"]],),
+                    "t4_14": no_yes_unknown(row[field_map["t4_14"]],),
+                    "t4_16": no_yes_unknown(row[field_map["t4_16"]],),
+                    "death_date": translate_date(row[field_map["death_date"]]),
                     "death_cause": get_and_check(
-                        row["cause_of_death"], PatientDetails.DEATH_CAUSES
+                        row[field_map["death_cause"]], PatientDetails.DEATH_CAUSES
                     ),
                 }
                 patient = Patient.objects.create()
