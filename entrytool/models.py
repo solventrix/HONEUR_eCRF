b4b0a637afd32703e91fed03ff98101d866a9eca
fredkingham <fred.kingham@openhealthcare.org.uk>
2020-10-07 11:03:47 +0100

    Adds in lost to follow up date which is required if the status is follow-up
    Adds a merge migration
    
    Merges the master migrations with translation migrations
    
    migration merge

--- entrytool/models.py
+++ entrytool/models.py
@@ -118,6 +118,9 @@ class PatientDetails(models.PatientSubrecord):
         blank=True,
         null=True,
     )
+    lost_to_follow_up_date = fields.DateField(
+        blank=True, null=True, verbose_name=_("Lost to Follow-up ")
+    )
 
     class Meta:
         verbose_name = _("Patient Details")
