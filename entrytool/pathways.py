bfd8905de1b33359a94bdbd663cafa61deb576ea
David Miller <david@deadpansincerity.com>
2020-10-07 19:29:34 +0100

    Add translations to add patient pathway

--- entrytool/pathways.py
+++ entrytool/pathways.py
@@ -7,7 +7,8 @@ from django.utils.translation import gettext_lazy as _
 class AddPatient(PagePathway):
     display_name = _("Add Patient")
     slug = "add_patient"
-#    icon = "fa-plus"
+    finish_button_text = _("Save")
+    icon = "fa "
 
     steps = [
         Step(model=models.Demographics, base_template='pathway/no_header_step.html')
