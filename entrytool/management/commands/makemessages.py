2abfd3d8bfdd63ed04cf9eb6946e2882aa0b94b5
fredkingham <fred.kingham@openhealthcare.org.uk>
2020-10-09 09:25:53 +0100

    reverting make messages changes as this is done by the translate substitution method

--- entrytool/management/commands/makemessages.py
+++ entrytool/management/commands/makemessages.py
@@ -6,22 +6,15 @@ import os
 
 from django.conf import settings
 from django.core.management.commands import makemessages as django_makemessages
-from django.db import models
 import jinja2
 import opal
 from opal.core import lookuplists
-from opal.core import subrecords
 
 FILENAME = os.path.join(settings.PROJECT_PATH, 'wow_strings.py')
 TEMPLATE = os.path.join(settings.PROJECT_PATH, 'templates/wow_strings.jinja2')
 
 REAL_OPAL = os.path.dirname(opal.__file__)
 OUR_OPAL  = os.path.join(settings.PROJECT_PATH, 'opal')
-FIELDS_TO_IGNORE = set([
-    "created", "created_by", "updated", "updated_by_id"
-    "episode_id", "patient_id", "consistency_token"
-])
-
 
 class Command(django_makemessages.Command):
 
@@ -35,27 +28,6 @@ class Command(django_makemessages.Command):
         for lookuplist in lookuplists.lookuplists():
             for item in lookuplist.objects.all():
                 strings.append(item.name)
-
-        # for subrecord in subrecords.subrecords():
-        #     for field in subrecord._get_fieldnames_to_serialize():
-        #         if field in FIELDS_TO_IGNORE:
-        #             continue
-        #         title = subrecord._get_field_title(field)
-        #         strings.append(
-        #             "{} is required".format(title)
-        #         )
-        #         if isinstance(getattr(subrecord, field), models.DateField):
-        #             strings.append(
-        #                 "{} is in th future".format(title)
-        #             )
-        #             if not title == "Date of Death":
-        #                 strings.append(
-        #                     "{} is after the date of death".format(title)
-        #                 )
-
-        # strings.append("Date is in the future")
-        # strings.append("Date is after the date of death")
-        # strings.append("Date is required")
 
         template = jinja2.Template(open(TEMPLATE, 'r').read())
         contents = template.render(strings=strings)
