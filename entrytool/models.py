058fd2c2a0d54daa1fd0b92aff76b223fdce7000
WoutV <w.vekemans@gmail.com>
2021-02-01 13:52:05 +0100

    adding IFT model

--- entrytool/models.py
+++ entrytool/models.py
@@ -82,6 +82,11 @@ class PatientDetails(models.PatientSubrecord):
         ("IgE", _("IgE")),
         ("Light Chain Myeloma", _("Light Chain Myeloma")),
     )
+    RISK_STRATIFICATIONS = (
+        ("Standard risk", _("Standard risk"),
+        ("Intermediate risk"), _("Intermediate risk"),
+        ("High risk"), _("High risk"))
+    )
     status = fields.CharField(
         max_length=100, choices=STATUSES, verbose_name=_("Patient Status")
     )
@@ -120,6 +125,7 @@ class PatientDetails(models.PatientSubrecord):
     lost_to_follow_up_date = fields.DateField(
         blank=True, null=True, verbose_name=_("Lost to Follow-up")
     )
+    risk_stratification = fields.CharField(max_length=25, choices = RISK_STRATIFICATIONS, verbose_name = _("Risk stratification"))
 
     class Meta:
         verbose_name = _("Patient Details")
@@ -208,6 +214,11 @@ class Response(models.EpisodeSubrecord):
         verbose_name = _("Response")
         verbose_name_plural = _("Responses")
 
+class Progression(models.EpisodeSubrecord):
+    _sort = "progression_date"
+    order_by = "-progression_date"
+    progression_date = fields.DateField(verbose_name=_("Progresion/relapse date"))
+
 
 class FollowUp(models.PatientSubrecord):
     _sort = "followup_date"
@@ -225,3 +236,65 @@ class FollowUp(models.PatientSubrecord):
     class Meta:
         verbose_name = _("Follow-up")
         verbose_name_plural = _("Follow-ups")
+
+class BloodCountFollowUp(models.PatientSubrecord):
+    _sort = "followup_date"
+    _icon  = "fa fa-stethoscope"
+    hospital = models.ForeignKeyOrFreeText(Hospital, verbose_name = _("Hospital"))
+    followup_date = fields.DateField(verbose_name=_("Visit date"))
+
+    erythrocytes = fields.FloatField(blank = True, null = True, verbose_name = _("erythrocytes"))
+    hemoglobin  = fields.FloatField(blank = True, null = True, verbose_name = _("hemoglobin"))
+    hematocrit = fields.FloatField(blank = True, null = True, verbose_name = _("hematocrit"))
+    mcv = fields.FloatField(blank = True, null = True, verbose_name = _("mcv"))
+    mch = fields.FloatField(blank = True, null = True, verbose_name = _("mch"))
+    mchc = fields.FloatField(blank = True, null = True, verbose_name = _("mchc"))
+    reticulocytes = fields.FloatField(blank = True, null = True, verbose_name = _("reticulocytes"))
+    leucocytes = fields.FloatField(blank = True, null = True, verbose_name = _("leucocytes"))
+    myeloblasts = fields.FloatField(blank = True, null = True, verbose_name = _("myeloblasts"))
+    promyelocytes = fields.FloatField(blank = True, null = True, verbose_name = _("promyelocytes"))
+    myelocytes = fields.FloatField(blank = True, null = True, verbose_name = _("myelocytes"))
+    eosinophils = fields.FloatField(blank = True, null = True, verbose_name = _("eosinophils"))
+    basophils = fields.FloatField(blank = True, null = True, verbose_name = _("basophils"))
+    lymphocytes = fields.FloatField(blank = True, null = True, verbose_name = _("lymphocytes"))
+    monocytes = fields.FloatField(blank = True, null = True, verbose_name = _("monocytes"))
+    plasmablasts = fields.FloatField(blank = True, null = True, verbose_name = _("plasmablasts"))
+    plasmacytes = fields.FloatField(blank = True, null = True, verbose_name = _("plasmacytes"))
+    platelets = fields.FloatField(blank = True, null = True, verbose_name = _("platelets"))
+
+    class Meta:
+        verbose_name = _("Blood Count")
+        verbose_name_plural = _("Blood Counts")
+
+
+MRD_CHOICES = (
+    #TODO fill with correct responses
+    ("Minimal", _("Minimal"))
+)
+class IFT(models.PatientSubrecord):
+    _sort = "ift_date"
+    ift_date = fields.DateField(verbose_name = _("IFT Date"))
+
+    YES_NO = (
+        ("Yes", _("Yes")),
+        ("No", _("No")),
+        ("Unknown", _("Unknown"))
+    )
+
+    MRD_status = fields.CharField(choices = MRD_CHOICES, blank = True, null = True, verbose_name = _("MRD status"))
+    percentage_tumor_cells = fields.FloatField(blank = True, null = True, verbose_name = _("Percentage of tumor cells by IFT"))
+    #TODO choices or freetext for phenotype? 
+    initial_tumor_phenotype = fields.FloatField(blank = True, null = True, verbose_name = _("Initial phenotype of tumor cells"))
+    phenotype = fields.FloatField(blank = True, null = True, verbose_name = _("Phenotype"))
+    cd38_138_presence = fields.CharField(choices = YES_NO, blank = True, null = True, verbose_name = _("CD38/CD138 presence"))
+    CD56_presence = fields.CharField(choices = YES_NO, blank = True, null = True, verbose_name = _("CD56 presence"))
+    CD19_presence = fields.CharField(choices = YES_NO, blank = True, null = True, verbose_name = _("CD19 presence"))
+    CD20_presence = fields.CharField(choices = YES_NO, blank = True, null = True, verbose_name = _("CD20 presence"))
+    CD27_presence = fields.CharField(choices = YES_NO, blank = True, null = True, verbose_name = _("CD27 presence"))
+    CD117_presence = fields.CharField(choices = YES_NO, blank = True, null = True, verbose_name = _("CD117 presence"))
+    CD38СD138_percentage = fields.FloatField(blank = True, null = True, verbose_name = _("CD38/СD138, %"))
+    CD56_percentage = fields.FloatField(blank = True, null = True, verbose_name = _("CD56, %"))
+    CD19_percentage = fields.FloatField(blank = True, null = True, verbose_name = _("CD19, %"))
+    CD20_percentage = fields.FloatField(blank = True, null = True, verbose_name = _("CD20, %"))
+    CD27_percentage = fields.FloatField(blank = True, null = True, verbose_name = _("CD27, %"))
+    CD117_percentage = fields.FloatField(blank = True, null = True, verbose_name = _("CD117 , %"))
