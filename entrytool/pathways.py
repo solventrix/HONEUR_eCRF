30891f0a0aa381d2c2c0ab9be9035963090fc2cd
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-02-17 14:52:58 +0000

    Save the start on the episode so that it will appear as the default episode on the patient detail rather than any treatment lines connected to the patient

--- entrytool/pathways.py
+++ entrytool/pathways.py
@@ -1,3 +1,4 @@
+import datetime
 from opal.core.pathway import PagePathway
 from opal.core.pathway.steps import Step
 from entrytool import models
@@ -9,6 +10,13 @@ class AddPatient(PagePathway):
     slug = "add_patient"
     finish_button_text = _("Save")
 #    icon = "fa-plus"
+
+    @transaction.atomic
+    def save(self, data, user, patient=None, episode=None):
+        patient, episode = super.save(data, user, patient, episode)
+        episode.start = datetime.date.today()
+        episode.save()
+        return patient, episode
 
     steps = [
         Step(model=models.Demographics, base_template='pathway/no_header_step.html')
