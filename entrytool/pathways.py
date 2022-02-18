9506c091c9e979c3194f0c284fe5a2f2e8f397cc
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-02-18 14:31:03 +0000

    Change the pathway to allow the user to select an episode category of MLL or CLL

--- entrytool/pathways.py
+++ entrytool/pathways.py
@@ -1,4 +1,4 @@
-import datetime
+from django.db import transaction
 from opal.core.pathway import PagePathway
 from opal.core.pathway.steps import Step
 from entrytool import models
@@ -12,5 +12,28 @@ class AddPatient(PagePathway):
 #    icon = "fa-plus"
 
     steps = [
-        Step(model=models.Demographics, base_template='pathway/no_header_step.html')
+        Step(model=models.Demographics, base_template='pathway/no_header_step.html'),
+        Step(
+            display_name='Category select',
+            template='pathway/category_select.html',
+            base_template='pathway/no_header_step.html'
+        ),
     ]
+
+    @transaction.atomic
+    def save(self, data, user, patient=None, episode=None):
+        """
+            If the patient already exists and has an infectious service
+            episode. Update that episode.
+
+            Else if the patient already exists, create a new episode.
+
+            Else if the patient doesn't exist load in the patient.
+        """
+        episode_category = data.pop('episode_category')
+        saved_patient, saved_episode = super().save(
+            data, user=user, patient=patient, episode=episode
+        )
+        saved_episode.category_name = episode_category[0]
+        saved_episode.save()
+        return saved_patient, saved_episode
