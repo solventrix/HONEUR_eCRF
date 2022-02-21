e11aec0182464dcb0f8ff7c86ba6490a70cbc127
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-02-21 10:28:42 +0000

    Rename the display name of the cll from CLL Condition to CLL because then we can use it in templates

--- plugins/conditions/cll/episode_categories.py
+++ plugins/conditions/cll/episode_categories.py
@@ -2,5 +2,5 @@ from opal.core import episodes
 
 
 class CLLCondition(episodes.EpisodeCategory):
-    display_name = "CLL Condition"
+    display_name = "CLL"
     detail_template = "detail/cll_condition.html"
