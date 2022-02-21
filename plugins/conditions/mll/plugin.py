52c29f7ebfac1f67ab979c457a3cb8bf20d4f4be
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-02-21 10:19:18 +0000

    Rename MLL to MM

--- plugins/conditions/mll/plugin.py
+++
@@ -1,8 +0,0 @@
-"""
-Plugin definition for plugins.conditions.mll
-"""
-from opal.core import plugins
-
-
-class MLLIndicatorPlugin(plugins.OpalPlugin):
-    pass
---
+++ plugins/conditions/mm/plugin.py
@@ -0,0 +1,8 @@
+"""
+Plugin definition for plugins.conditions.mm
+"""
+from opal.core import plugins
+
+
+class MMIndicatorPlugin(plugins.OpalPlugin):
+    pass
