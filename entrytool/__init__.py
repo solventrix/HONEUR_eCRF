45344eefa71ec770667d189b51019c814f665ae8
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-04-28 16:57:22 +0100

    Updates the app to use the lastest version of opal
    It also updates other dependencies

--- entrytool/__init__.py
+++ entrytool/__init__.py
@@ -29,9 +29,6 @@ class Application(application.OpalApplication):
 
     @classmethod
     def get_menu_items(klass, user=None):
-        # we import here as settings must be set before this is imported
-        from django.contrib.auth.views import logout as logout_view
-
         menuitems = []
         if user:
             if user.is_authenticated:
@@ -42,7 +39,7 @@ class Application(application.OpalApplication):
                         activepattern='/pathway/#/add_patient'
                     ),
                     menus.MenuItem(
-                        href=reverse(logout_view), display=_('Log Out'), index=1000
+                        href=reverse("logout"), display=_('Log Out'), index=1000
                     )
                 ]
                 if user.is_staff:
