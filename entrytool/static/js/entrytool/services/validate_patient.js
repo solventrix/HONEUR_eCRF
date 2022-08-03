a190673660663fe0fd743293df0c3905b43f69b7
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-08-03 20:45:27 +0100

    Adds in an empty validate patient service

---
+++ entrytool/static/js/entrytool/services/validate_patient.js
@@ -0,0 +1,10 @@
+angular.module('opal.services').factory('ValidatePatient', function() {
+	return {
+		validatePatientFromId: function(patientId){
+			return [];
+		},
+		validatePatient: function(patient){
+			return [];
+		}
+	}
+});
