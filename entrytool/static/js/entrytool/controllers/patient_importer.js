cb54d057c1fcbce098bd01b5d8c75532e4560136
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-08-05 12:35:24 +0100

    Change the patient importer to iterate over each patient and validate them in turn

--- entrytool/static/js/entrytool/controllers/patient_importer.js
+++ entrytool/static/js/entrytool/controllers/patient_importer.js
@@ -3,13 +3,25 @@ angular
   .controller("PatientImporter", function (
     $scope, $http, $q, patientLoader, ValidatePatient
   ) {
+    "use strict";
 
+    // a list of unvalidate patient ids
     $scope.unvalidatedPatients = [];
+
+    // a list of patient ids with errors
     $scope.patientsWithErrors = [];
-    $scope.initialUnvalidatedCount = null;
+
+    // the initial number of unvalidated patients
+    $scope.initialUnvalidatedCount = 0;
+
+    // the number of patients we have currently validated
     $scope.validaterPointer = 0;
 
     var init = function(){
+      /*
+      * Load in all unvalidated patients and patients with errors.
+      * Iterate over the unvalidated patients and start validating them.
+      */
       var unValidatedPatientsPromise = $http.get('/entrytool/v0.1/unvalidated_patients/');
       var patientsWithErrorsPromise = $http.get('/entrytool/v0.1/patients_with_errors/');
       $q.all([unValidatedPatientsPromise, patientsWithErrorsPromise]).then(function(result){
@@ -20,14 +32,33 @@ angular
       })
     }
 
+    var processPatientId = function(unvalidatedPatientId){
+      /*
+      * Take an unvalidated patient, load the patient, validate them and
+      * update the pointer and the patientWithErrors if necessary.
+      */
+      var deferred = $q.defer()
+      patientLoader(unvalidatedPatientId).then(ValidatePatient.validatePatient).then(function(errors){
+        if(errors.length){
+          $scope.patientsWithErrors.push(unvalidatedPatientId)
+        }
+        $scope.validaterPointer += 1;
+        deferred.resolve()
+      });
+      return deferred.promise;
+    }
+
     var process = function(){
+      /*
+      * Iterate through all unvalidated patients and process them one after
+      * another.
+      */
+      var deferred = $q.defer()
+      deferred.resolve();
+      var promise = deferred.promise;
       _.each($scope.unvalidatedPatients, function(unvalidatedPatientId){
-        patientLoader(unvalidatedPatientId).then(ValidatePatient.validatePatient).then(function(errors){
-          if(errors.length){
-            $scope.patientsWithErrors.push(unvalidatedPatientId)
-          }
-          $scope.validaterPointer += 1;
-        });
+        // chain the promises so that we resolve them one after another
+        promise = promise.then(function(){ return processPatientId(unvalidatedPatientId)});
       });
     }
 
