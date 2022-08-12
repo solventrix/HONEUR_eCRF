5dfeb9b9862214a14864a0c2bd867c43ff6eaaa2
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-08-12 11:15:15 +0100

    Remove the validateField function in the PatientValidator. This is never used, insteads we use ValidateField.validate

--- entrytool/static/js/entrytool/controllers/patient_validator.js
+++ entrytool/static/js/entrytool/controllers/patient_validator.js
@@ -17,33 +17,6 @@ angular
       self.warnings = {};
       self.patient = patient;
 
-      this.validateField = function(apiName, fieldName, value, instance, episode, patient){
-        /*
-        * validates for different alertTypes which are either warnings or errors
-        */
-        var result = {
-          warnings: [],
-          errors: []
-        }
-        var self = this;
-        if(self[apiName] && self[apiName][fieldName]){
-          _.each(['warnings', 'errors'], function(alertType){
-            if(!ValidationRules[apiName][fieldName][alertType]){
-              return;
-            }
-            _.each(ValidationRules[apiName][fieldName][alertType], function(validator_and_error){
-              var validator = validator_and_error[0];
-              var error = validator_and_error[1];
-              var issue = validator(value, instance, episode, patient);
-              if(issue){
-                result[alertType].push(error);
-              }
-            });
-          })
-        }
-        return result;
-      }
-
       this.validatesubrecord = function(subrecordApiName, subrecord, episode){
         /*
         * Validates a whole subrecord and sets the errors/warnings on itself.errors/warnings
