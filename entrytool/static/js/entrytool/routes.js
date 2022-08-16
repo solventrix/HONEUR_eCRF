7d4dfa7053a338abbc0c0c578e4233907a992d9c
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-08-16 17:39:26 +0100

    Fixes merge conflicts with construct-load-errors

--- entrytool/static/js/entrytool/routes.js
+++ entrytool/static/js/entrytool/routes.js
@@ -11,21 +11,10 @@
                controller: 'WelcomeCtrl',
                templateUrl: '/templates/welcome.html'}
                               )
+
+
           $routeProvider.when('/#/patient',  {
             controller: 'AddEpisodeCtrl'}
-                            )
-
-          $routeProvider.when('/data_upload', {
-            controller: 'DataUploader',
-            templateUrl: '/templates/data_upload.html',
-            resolve: {
-              unValidatedPatients: function(DataUploadLoader){
-                return DataUploadLoader.unValidatedPatients()
-              },
-              patientsWithErrors: function(DataUploadLoader){
-                return DataUploadLoader.patientsWithErrors()
-              }
-            }
-          })
+                            )                   
        }]);
 })();
