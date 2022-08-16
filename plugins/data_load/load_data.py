7316c51595748a3971eebf422576b591b250e524
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-08-16 11:30:59 +0100

    Adds error handling for the data upload if the folder/files don't exist

--- plugins/data_load/load_data.py
+++ plugins/data_load/load_data.py
@@ -14,18 +14,40 @@ def load_data(folder):
         with transaction.atomic():
             errors = _load_data(folder)
             if errors:
-                raise LoadError('rolling back transaction')
+                raise LoadError("rolling back transaction")
     except LoadError:
         pass
     return errors
 
 
 def _load_data(folder):
+    if not os.path.exists(folder):
+        return [
+            dict(
+                file="N/A",
+                row="N/A",
+                column="N/A",
+                value=folder,
+                short_description="Unable to find folder on disk",
+                traceback=["Please enter a file path to a folder on disk"],
+            )
+        ]
     files = os.listdir(folder)
 
+    unable_to_find_file_errors = []
     for file_name in ["demographics.csv", "lot.csv", "follow_ups.csv"]:
         if file_name not in files:
-            raise ValueError(f'Unable to find {file_name} in {options["folder"]}')
+            file_path = os.path.join(folder, file_name)
+            unable_to_find_file_errors.append(dict(
+                file="N/A",
+                row="N/A",
+                column="N/A",
+                value=file_name,
+                short_description=f"Unable to find a file on disk at {file_path}",
+                traceback=[f"{file_name} must already exist on disk"],
+            ))
+    if len(unable_to_find_file_errors):
+        return unable_to_find_file_errors
 
     demographics_loader = load_demographics.DemographicsLoader(
         os.path.join(folder, "demographics.csv"),
