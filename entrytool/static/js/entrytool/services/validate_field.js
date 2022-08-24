31dfa5dccb2e4f9d473d6807e4f1a253f1e24b45
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-08-24 22:05:24 +0100

    Only show the first error/warning
    Some errors are not mutually exclusive for example overlapping regimen
    and overlapping LOT so just show the first one so that the UI doesn't
    get cluttered

--- entrytool/static/js/entrytool/services/validate_field.js
+++ entrytool/static/js/entrytool/services/validate_field.js
@@ -11,6 +11,10 @@ angular.module('opal.services').service('ValidateField', function(ValidationRule
 			if(ValidationRules[apiName] && ValidationRules[apiName][fieldName]){
 				_.each(ValidationRules[apiName][fieldName], function(alertFunctions, alertType){
 					_.each(alertFunctions, function(validator_and_error){
+						// only return the first warning/error
+						if(result[alertType].length){
+							return;
+						}
 						var validator = validator_and_error[0];
 						var error = validator_and_error[1];
 						var isValid = validator(value, instance, episode, patient, apiName, fieldName, schema, lookuplists);
