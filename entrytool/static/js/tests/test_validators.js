caa466edd460b8b24135a5963f5e40a3aaf94380
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-08-24 11:19:34 +0100

    Fixes the CLLRegimenRequired unit tests

--- entrytool/static/js/tests/test_validators.js
+++ entrytool/static/js/tests/test_validators.js
@@ -598,12 +598,12 @@ describe("Validators", function () {
 	});
 
 	describe('CLLRegimenRequired', function(){
-		it('should return false for an empty regimen if the category is not Treatment', function(){
-			expect(Validators.CLLRegimenRequired(undefined, {category: 'Other'})).toBe(false);
+		it('should return true for an empty regimen if the category is not Treatment', function(){
+			expect(Validators.CLLRegimenRequired(undefined, {category: 'Other'})).toBe(true);
 		});
 
-		it('should return true for an empty regimen if the category is Treatment', function(){
-			expect(Validators.CLLRegimenRequired(undefined, {category: 'Treatment'})).toBe(true);
+		it('should return false for an empty regimen if the category is Treatment', function(){
+			expect(Validators.CLLRegimenRequired(undefined, {category: 'Treatment'})).toBe(false);
 		});
 	});
 
